04ea4c1b166d98f57744680e6e3f9302d7b0bb44
Christopher Brown <io@henrian.com>
2012-10-05 02:51:12 -0500

    Set other headers in curl

--- remoting/curl.py
+++ remoting/curl.py
@@ -1,7 +1,7 @@
 import pycurl
 import StringIO
 
-def curl(url, socks_host=None, socks_port=None):
+def curl(url, headers=None, socks_host=None, socks_port=None):
     # if socks_port is given, it should be an integer
     c = pycurl.Curl()
     c.setopt(pycurl.URL, url)
@@ -10,6 +10,10 @@ def curl(url, socks_host=None, socks_port=None):
         c.setopt(pycurl.PROXY, socks_host or 'localhost')
         c.setopt(pycurl.PROXYPORT, socks_port or 5090)
         c.setopt(pycurl.PROXYTYPE, pycurl.PROXYTYPE_SOCKS5)
+
+    if headers:
+        header_list = ['%s: %s' % (key, val) for key, val in headers.items()]
+        c.setopt(pycurl.HTTPHEADER, header_list)
 
     output = StringIO.StringIO()
     c.setopt(pycurl.WRITEFUNCTION, output.write)
